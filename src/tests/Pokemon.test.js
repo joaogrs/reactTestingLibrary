f94071299b08c1fb6b2b3510cec6f8d95ee0a848
jaogrs <jgabrielrodrigues2@gmail.com>
2022-02-21 11:06:04 -0300

    requisito 5 correcao 3

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { screen, render } from '@testing-library/react';
 import { Router } from 'react-router-dom';
-import userEvent from '@testing-library/user-event';
 import { createMemoryHistory } from 'history';
 import App from '../App';
 
